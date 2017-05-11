fdd6531352d07ff4e04eef17343265c62f14bf71
ckingchris <christopherkingucla@gmail.com>
2017-05-10 21:03:15 -0700

    additional fixes for the game

--- assets/js/hangman.js
+++ assets/js/hangman.js
@@ -1,8 +1,8 @@
 // Array of disney movies options
-var options = ["moana", "lion king", "finding nemo", "aladdin", "mulan", "jungle book", "frozen", "tarzan", "big hero 6"];
+var options = ["moana", "aladdin", "mulan", "frozen", "tarzan", "cars", "lionking"];
 
 // Number of guesses
-var left = 18;
+var left = 15;
 document.getElementById("left").innerHTML = left;
 
 // Wins counter
@@ -35,11 +35,12 @@ window.onload = function() {
 }
 
 function init() {
+    document.getElementById("youwin").innerHTML = '';
     blankword = [];
     wrongArray = [];
     document.getElementById("wrong").innerHTML = wrongArray.join(" , ");
     userArray = [];
-    left = 18;
+    left = 15;
     document.getElementById("left").innerHTML = left;
 
     // Randomly select word from array 
@@ -67,7 +68,6 @@ document.onkeyup = function(input) {
             document.getElementById("left").innerHTML = left;
             if (left === 0) {
                 alert("Game Over!")
-                start = false;
                 init();
             }
         }
@@ -76,39 +76,50 @@ document.onkeyup = function(input) {
             var match = false;
 
             for (var i = 0; i < length; i++) {
-                if (word[i] === " ") {
-                    blankword[i] = "&nbsp;";
-                }
+                // if (word[i] === " ") {
+                //     blankword[i] = "&nbsp;";
+                // }
                 if (userInput === word[i]) {
                     blankword[i] = userInput;
                     userArray.push(userInput);
                     match = true;
                     document.getElementById("display").innerHTML = blankword.join(" ")
-                    } else {
-                        match = false;
-                    }
+                    } 
                 }
             
             if (match === false) {
                 if (wrongArray.includes(userInput)) {
                     alert("You already guessed that")
-                    wrongArray.push(userInput);
                 } else {
                     wrongArray.push(userInput);
-                    // console.log(wrongArray)
                     document.getElementById("wrong").innerHTML = wrongArray.join(" , ");
                 }
             }
-
-            // If userArray length = word length player wins
-            if (userArray.length === word.length) {
-                wins++;
-                document.getElementById("display").innerHTML = blankword.join(" ")
-                document.getElementById("wins").innerHTML = wins;
-                alert("You win!");
-                init();
-            }
-        } else {
+        } else if (input.which == '13') {
+            init();
+        }
+        else {
             alert("Only use keys a-z");
         }
+        // If userArray length = word length player wins           
+        if (userArray.length === word.length) {  
+            wins++;
+            document.getElementById("wins").innerHTML = wins;
+            document.getElementById("display").innerHTML = blankword.join(" ")
+            document.getElementById("youwin").innerHTML = 'You win! Press enter to start the next game.';
+                // if (input.keyCode == '13') {
+                //     init();
+                // }
+        } 
+            // if (userArray.join() == word) {
+            //     document.getElementById("display").innerHTML = blankword.join(" ")
+            //         document.getElementById("wins").innerHTML = wins;
+            //         document.getElementById("youwin").innerHTML = 'You win! Press enter to start the next game.';
+            //     if (input.keyCode == '13') {
+            //         wins++;
+            //         userArray = [];
+            //         word = [];
+            //         init();
+            //     }
+            // }       
 }
